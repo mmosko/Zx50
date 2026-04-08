905b0441edf4726b406820bbc9348f92c2074d73
Marc Mosko <marc@mosko.org>
2026-04-08 15:34:45 -0700

    add script to program atf1509

---
+++ model/syn/program.sh
@@ -0,0 +1,9 @@
+marc@morska:~/PycharmProjects/Zx50/model/syn$ sudo openocd \
+  -c "adapter driver ch347" \
+  -c "ch347 vid_pid 0x1a86 0x55dd" \
+  -c "adapter speed 1000" \
+  -c "jtag newtap atf1508 tap -irlen 10 -expected-id 0x0150803f" \
+  -c "init" \
+  -c "svf zx50_cpld_core.svf" \
+  -c "shutdown"
+
