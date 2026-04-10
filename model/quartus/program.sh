00be9db12cf525268b4cda83231469202c29520f
Marc Mosko <marc@mosko.org>
2026-04-09 20:52:23 -0700

    make scripts x and not sudo

--- model/quartus/program.sh
+++ model/quartus/program.sh
@@ -1,4 +1,4 @@
-sudo openocd -c "adapter driver ch347" \
+openocd -c "adapter driver ch347" \
   -c "ch347 vid_pid 0x1a86 0x55dd" \
   -c "adapter speed 1000" \
   -c "jtag newtap atf1508 tap -irlen 10 -expected-id 0x0150803f" \
